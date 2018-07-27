636cf59d313be9b4721a4b7057ba4354890842bc
dreamflown <821981739@qq.com>
2018-07-28 00:49:03 +0800

    张汉森

--- src/main/java/com/bupt/wechatplugin/Application.java
+++ src/main/java/com/bupt/wechatplugin/Application.java
@@ -3,12 +3,14 @@ package com.bupt.wechatplugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import com.bupt.wechatplugin.domain.*;
 
-@SpringBootApplication
+
+@SpringBootApplication  //等价于以默认属性使用@Configuration，@EnableAutoConfiguration和@ComponentScan
 /*@PropertySource({"classpath:disconf.properties"})
 @ImportResource({"classpath:disconf.xml"})//引入disconf*/
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
